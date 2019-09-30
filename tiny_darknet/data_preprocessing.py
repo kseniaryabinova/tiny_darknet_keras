1e4869082baa0562c1a3e5b524248bcb931134cf
Ksenya Ryabinova <ryabinova.ka@mail.ru>
2019-09-30 13:32:10 +0300

    changed example.yml

--- tiny_darknet/data_preprocessing.py
+++ tiny_darknet/data_preprocessing.py
@@ -11,13 +11,13 @@ def preprocess_train_data(x, y):
     x_train /= 255
 
     datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        zca_epsilon=1e-06,  # epsilon for ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        # featurewise_center=False,  # set input mean to 0 over the dataset
+        # samplewise_center=False,  # set each sample mean to 0
+        # featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        # samplewise_std_normalization=False,  # divide each input by its std
+        # zca_whitening=False,  # apply ZCA whitening
+        # zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=7,  # randomly rotate images in the range (degrees, 0 to 180)
         # randomly shift images horizontally (fraction of total width)
         width_shift_range=0.1,
         # randomly shift images vertically (fraction of total height)
